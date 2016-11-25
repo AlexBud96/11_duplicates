cc4bff224c65f492c2040ef7dfcadb4fdf2b051f
AlexBud96 <AlexSy4ov@gmail.com>
2016-11-25 18:47:07 +0300

    версия 1.01

--- duplicates.py
+++ duplicates.py
@@ -2,22 +2,22 @@ import os
 from os.path import join, getsize
 
 def obj_into_lists(file_path):
-    Roots = []
-    Names = []
-    Sizes = []
+    roots = []
+    names = []
+    sizes = []
     for root, dirs, files in os.walk(file_path):
         for file in files:
-            Roots.append(root)
-            Names.append(file)
-            Sizes.append(getsize(join(root, file)))
-    return Roots, Names, Sizes
+            roots.append(root)
+            names.append(file)
+            sizes.append(getsize(join(root, file)))
+    return roots, names, sizes
 
-def are_files_duplicates(Roots, Names, Sizes):
+def are_files_duplicates(roots, names, sizes):
     overlooked = []
     duplicates = []
-    for i in range(len(Names)):
-        for j in range(len(Names)):
-            if (i != j) and (overlooked.count(i) == 0) and (overlooked.count(j) == 0) and (Names[i] == Names[j]) and (Sizes[i] == Sizes[j]):
+    for i in range(len(names)):
+        for j in range(len(names)):
+            if (i != j) and (overlooked.count(i) == 0) and (overlooked.count(j) == 0) and (names[i] == names[j]) and (sizes[i] == sizes[j]):
                 overlooked.append(j)
                 overlooked.append(i)
                 duplicates.append([j, i])
@@ -25,15 +25,15 @@ def are_files_duplicates(Roots, Names, Sizes):
 
 def print_duplicates(items):
     for item in items:
-        print('Файл с именем %s, размером %dкб, находящийся в директории %s' % (Names[item[0]], Sizes[item[0]], join(Roots[item[0]], Names[item[0]])))
-        print('является дубликатом файла с именем %s, размером %dкб, находящимся в директории %s' % (Names[item[1]], Sizes[item[1]], join(Roots[item[1]], Names[item[1]])))
+        print('Файл с именем %s, размером %dкб, находящийся в директории %s' % (names[item[0]], sizes[item[0]], join(roots[item[0]], names[item[0]])))
+        print('является дубликатом файла с именем %s, размером %dкб, находящимся в директории %s' % (names[item[1]], sizes[item[1]], join(roots[item[1]], names[item[1]])))
         print('----------------------------------------------------------------------------------------------')
 
 if __name__ == '__main__':
     file_path = input('Введите путь до директории, чтобы найти там дубликаты: ')
     lists = obj_into_lists(file_path)
-    Roots = lists[0]
-    Names = lists[1]
-    Sizes = lists[2]
-    duplicates = are_files_duplicates(Roots, Names, Sizes)
+    roots = lists[0]
+    names = lists[1]
+    sizes = lists[2]
+    duplicates = are_files_duplicates(roots, names, sizes)
     print_duplicates(duplicates)
